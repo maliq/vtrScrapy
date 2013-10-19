8614ec438fbfb0f3163a7784d545e4f812382d05
Marcelo Aliquintuy <mmaliq@gmail.com>
2013-10-18 22:18:50 -0300

    update to crawler to v0.18 scrapy

--- crawler_worker.py
+++ crawler_worker.py
@@ -1,8 +1,9 @@
 from scrapy import project, signals
-from scrapy.crawler import CrawlerProcess
+from scrapy.crawler import Crawler
 from scrapy.xlib.pydispatch import dispatcher
 from multiprocessing.queues import Queue
 from scrapy.settings import Settings
+from twisted.internet import reactor
 import multiprocessing
 
 class CrawlerWorker(multiprocessing.Process):
@@ -11,20 +12,22 @@ class CrawlerWorker(multiprocessing.Process):
         multiprocessing.Process.__init__(self)
         self.result_queue = result_queue
 
-        self.crawler = CrawlerProcess(Settings())
-        if not hasattr(project, 'crawler'):
-            self.crawler.install()
+        self.crawler = Crawler(Settings())
         self.crawler.configure()
 
         self.items = []
         self.spider = spider
         dispatcher.connect(self._item_passed, signals.item_passed)
+        dispatcher.connect(self._stop_reactor, signal=signals.spider_closed)
 
     def _item_passed(self, item):
         self.items.append(item)
 
+    def _stop_reactor(self):
+        reactor.stop()
+
     def run(self):
         self.crawler.crawl(self.spider)
         self.crawler.start()
-        self.crawler.stop()
+        reactor.run()
         self.result_queue.put(self.items)
